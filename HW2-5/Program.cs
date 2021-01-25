8c11a9709a3b7e98bba3db6b7e6789738e618108
Semenow94 <semenow94@gmail.com>
2021-01-25 17:54:12 +0300

    ДЗ 2-5

---
+++ HW2-5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HW2_5
+{
+    //Если пользователь указал месяц из зимнего периода, а средняя температура > 0, вывести сообщение «Дождливая зима»
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите среднюю температуру : ");
+            double temp = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите порядковый номер текущего месяца : ");
+            int monthNumber = Convert.ToInt32(Console.ReadLine());
+            switch (monthNumber)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    if (temp > 0) Console.WriteLine("Дождливая зима");
+                    break;
+                default:
+                    Console.WriteLine("Такого месяца нет");
+                    break;
+            }
+        }
+    }
+}
