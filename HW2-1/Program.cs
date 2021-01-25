e1f1c19a7ef99f4e8754be1331d823a848971960
Semenow94 <semenow94@gmail.com>
2021-01-25 16:20:16 +0300

    ДЗ 2-1

---
+++ HW2-1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HW2_1
+{
+    //Запросить у пользователя минимальную и максимальную температуру за сутки и вывести среднесуточную температуру.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите температуру утром : ");
+            double temp1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите температуру вечером : ");
+            double temp2 = Convert.ToDouble(Console.ReadLine());
+            double averageTemp = (temp1 + temp2) / 2;
+            Console.WriteLine("Средняя температура за день "+averageTemp);
+            Console.ReadLine();
+        }
+    }
+}
