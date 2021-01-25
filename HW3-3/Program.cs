d1513cba11b5f0278fc6595ee2c742a7861ae877
Semenow94 <semenow94@gmail.com>
2021-01-25 18:51:21 +0300

    ДЗ 3-3

---
+++ HW3-3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HW3_3
+{
+    //Написать программу, выводящую введенную пользователем строку в обратном порядке(olleH вместо Hello).
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите любое слово :");
+            string word = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("Ваше слово наоборот : ");
+            RemakeWord(word);
+        }
+        static void RemakeWord(string word)
+        {
+            for (int i = word.Length - 1; i >= 0; i--) Console.Write(word[i]+" ");
+        }
+    }
+}
