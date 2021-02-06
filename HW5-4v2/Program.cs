b3c76711f7478d0c902878841d7c86425c7644eb
Semenow94 <semenow94@gmail.com>
2021-02-06 19:14:43 +0300

    ДЗ 5-4 в2

---
+++ HW5-4v2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace HW5_4v2
+{
+    //Сохранить дерево каталогов и файлов по заданному пути в текстовый файл — с рекурсией и без.
+    //Без рекурсии и без использования стандартных методов
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите полное название директории : ");
+            string dir = Console.ReadLine();
+            string textFile = "Folders.txt";
+            List<string> fileList = new List<string>();
+            if (Directory.Exists(dir))
+            {
+                WriteFiles(dir, ref fileList);
+                File.WriteAllLines(textFile, fileList);
+            }
+            else
+            {
+                Console.WriteLine("Такой директории не существует");
+            }
+        }
+        static void WriteFiles(string dirName, ref List<string> fileList)
+        {
+            bool flag = true;
+            int i = 0;
+            fileList.Add(dirName);
+            while(flag)
+            {
+                List<string> addToBaseList = new List<string>();
+                if (Directory.Exists(fileList[i]))
+                {
+                    addToBaseList.AddRange(Directory.GetDirectories(fileList[i]));
+                    addToBaseList.AddRange(Directory.GetFiles(fileList[i]));
+                    fileList.InsertRange(i+1,addToBaseList);
+                }
+                i++;
+                if (i > fileList.Count - 1) flag = false;
+            }
+        }
+    }
+}
